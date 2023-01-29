b72fb0a1f75bb0b75d088c9e474b9c2c4e244d3f
Alvaro Angel <86565212+bitnibelun@users.noreply.github.com>
2023-01-29 18:12:19 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarMake, CarModel, CarDealer
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -61,12 +61,27 @@ def registration_request(request):
         return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+# def get_dealerships(request):
+#     context = {}
+#     if request.method == "GET":
+#         print('Here: get_dealerships')
+#         return render(request, 'djangoapp/index.html', context)
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        print('Here: get_dealerships')
-        return render(request, 'djangoapp/index.html', context)
-
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/b611b81f-938e-43c4-965c-f566c4721a29/dealership-package/get-dealer-sequence.json"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
+# def get_dealerships(request):
+#     if request.method == "GET":
+#         context = {}
+#         url = "https://us-south.functions.appdomain.cloud/api/v1/web/b611b81f-938e-43c4-965c-f566c4721a29/dealership-package/get-dealer-sequence"
+#         dealerships = get_dealers_from_cf(url)
+#         context["dealership_list"] = dealerships
+#         return render(request, 'djangoapp/index.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
